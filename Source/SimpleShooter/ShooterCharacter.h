74e7ba1c3fef7a4879d1ddc16ce1b2f8e5ec7c2e
JIbald <45597910+JIbald@users.noreply.github.com>
2020-08-17 18:21:20 +0200

    added a Health variable (ShooterCharacter), TakeDamage function, Damage Variable (per Gun object), IsDead() function (ShooterCharacter) and death animation (shot from front)

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -22,11 +22,16 @@ protected:
 	virtual void BeginPlay() override;
 
 public:	
+	UFUNCTION(BlueprintPure)
+		bool IsDead() const;
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
 private:
 	void MoveForward(float AxisValue);
@@ -38,8 +43,15 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float PitchRate{ 30 }; //rotation rate for looking up/down
+	
 	UPROPERTY(EditAnywhere)
 	float YawRate{ 30 }; //rotation rate for looking left/right
+
+	UPROPERTY(EditDefaultsOnly)
+	float MaxHealth{ 100.f };
+
+	UPROPERTY(VisibleAnywhere)
+	float Health;
 
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<AGun> GunClass;
