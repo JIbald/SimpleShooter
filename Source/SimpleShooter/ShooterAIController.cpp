37398b0aad7dff8c8840bf8726dd6ee15d9dc226
JIbald <45597910+JIbald@users.noreply.github.com>
2020-08-18 19:23:30 +0200

    enemy follows player, investigates last seen location for 5s, walks back to starting location.

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -12,11 +12,9 @@ void AShooterAIController::BeginPlay()
 	if (AIBehavior != nullptr)
 	{
 		RunBehaviorTree(AIBehavior);
-		APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 		//FVector StartLocation = GetPawn()->GetPawnViewLocation(); //worked as well but not sure of difference
 		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation() );
-		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation() );
-
 	}
 }
 
@@ -36,4 +34,18 @@ void AShooterAIController::Tick(float DeltaSeconds)
 	//	ClearFocus(EAIFocusPriority::Gameplay);
 	//	StopMovement();
 	//}
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (LineOfSightTo(PlayerPawn) )
+	{
+		// Setting PlayerLocation
+		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation() );
+		// Setting lastknown
+		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation() );
+	}
+	else
+	{
+		// clear PlayerLocation
+		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	}
 }
