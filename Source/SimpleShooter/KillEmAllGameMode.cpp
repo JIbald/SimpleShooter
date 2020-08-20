05d3f223a7f75a4e23e7c2c1fa9c5de471c35373
JIbald <45597910+JIbald@users.noreply.github.com>
2020-08-20 18:32:15 +0200

    game ends and restart

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -6,6 +6,11 @@
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled(PawnKilled);
+	//UE_LOG(LogTemp, Warning, TEXT("A Pawn was killed"));
 
-	UE_LOG(LogTemp, Warning, TEXT("A Pawn was killed"));
+	APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
+	if (PlayerController != nullptr)
+	{
+		PlayerController->GameHasEnded(nullptr, false);
+	}
 }
